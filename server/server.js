3742812727b643aadc432510cf34b11b1dc0756d
h1692 <tc_hualingfeng@163.com>
2021-08-11 20:12:53 +0800

    static 资源引入

--- server/server.js
+++ server/server.js
@@ -11,8 +11,7 @@ const app = new Koa()
 const router = Router()
 const PORT = 9000
 
-// app.use(views(path.join(__dirname,"../build/"),{extension:'html'}))
-
+app.use(require('koa-static')(path.resolve('./build'))) // 静态资源地址配置
 router.get('/',async(ctx)=>{
   const data = fs.readFileSync(path.resolve('./build/index.html'),'utf-8')
   ctx.body = data.replace('<div id="root"></div>', `<div id="root">${ renderToString(<App />) }</div>`)
